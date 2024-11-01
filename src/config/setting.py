7ada007822fe61ecf9c513aa2cefc348f146e4ae
van-artist <shangliu385@gmail.com>
2024-11-01 20:58:01 +0800

    init commit

---
+++ src/config/setting.py
@@ -0,0 +1,8 @@
+GAME_SETTINGS={
+    "board_size": 8,
+    "symbol": {
+        "empty": ".",
+        "black": "X",
+        "white": "O"
+    }
+}
---
+++ src/main.py
@@ -0,0 +1,7 @@
+from core.board import Board
+
+
+if __name__ == '__main__':
+    board = Board()
+    board.display()
+    
